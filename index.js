c476eb9181ba6a36752cbb3d584883d7028e814c
Hafsa Malik <hafsamalik322@gmail.com>
2022-10-07 21:09:58 +0500

    JS css edit

--- index.js
+++ index.js
@@ -41,35 +41,57 @@ document.querySelector("#close").addEventListener("click", function () {
     document.querySelector(".popup").style.display = "none";
 });
 
-const numb1 = document.querySelector(".numb1");
-let counter1 = 0;
-setInterval(()=>{
-if(counter1 == 134){
-clearInterval();
-}else{
-counter1+=1;
-numb1.textContent = counter1;
-}
-}, 15);
 
-const numb2 = document.querySelector(".numb2");
-let counter2 = 0;
-setInterval(()=>{
-if(counter2 == 250){
-clearInterval();
-}else{
-counter2+=1;
-numb2.textContent = counter2;
-}
-}, 5);
+//stats counter animation
+const scrollOffset = 100;
+const scrollElement = document.querySelector("#goals");
 
-const numb3 = document.querySelector(".numb3");
-let counter3 = 0;
-setInterval(()=>{
-if(counter3 == 75){
-clearInterval();
-}else{
-counter3+=1;
-numb3.textContent = counter3;
+const elementInView = (el, offset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        ((window.innerHeight || document.documentElement.clientHeight) - offset)
+    );
+};
+
+window.addEventListener('scroll', () => {
+    if (elementInView(scrollElement, scrollOffset)) {
+        changeStats();
+    }
+})
+
+function changeStats() {
+    const numb1 = document.querySelector(".numb1");
+    let counter1 = 0;
+    setInterval(() => {
+        if (counter1 == 134) {
+            clearInterval();
+        } else {
+            counter1 += 1;
+            numb1.textContent = counter1;
+        }
+    }, 15);
+
+    const numb2 = document.querySelector(".numb2");
+    let counter2 = 0;
+    setInterval(() => {
+        if (counter2 == 250) {
+            clearInterval();
+        } else {
+            counter2 += 1;
+            numb2.textContent = counter2;
+        }
+    }, 5);
+
+    const numb3 = document.querySelector(".numb3");
+    let counter3 = 0;
+    setInterval(() => {
+        if (counter3 == 75) {
+            clearInterval();
+        } else {
+            counter3 += 1;
+            numb3.textContent = counter3;
+        }
+    }, 25);
 }
-}, 25);
